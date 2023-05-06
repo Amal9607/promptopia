f047e4640510d380a39c145cde8e70b0b4491805
Amal221200 <amalmurikkoli2000@gmail.com>
2023-05-06 18:09:53 +0530

    create-prompt backend

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,3 +1,16 @@
-export async function POST(request) {
+import Prompt from "@/models/prompt"
+import { connectToDB } from "@/utils/database"
 
+export async function POST(req, res) {
+    const { userId, prompt, tag } = await req.json()
+
+    try {
+        await connectToDB()
+        const newPrompt = new Prompt({ creator: userId, tag, prompt })
+        await newPrompt.save()
+
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
+    } catch (error) {
+        return new Response('Failed to create a new prompt', { status: 500 })
+    }
 }
